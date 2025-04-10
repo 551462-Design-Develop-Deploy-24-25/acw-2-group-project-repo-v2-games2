e04fc83cc9864b37e1ab371e75a45783db83965b
Jack Ballard <jacksballard@gmail.com>
2025-04-10 01:13:03 +0100

    added unversioned files

---
+++ DDD_Game/Assets/Scripts/NavigationScript.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+
+
+public class NavigationScript : MonoBehaviour
+{
+    private enum State
+    {
+        Roam, // Wide area wander
+        Search, // Small area wander
+        Chase // Target player
+
+    }
+
+    public GameObject player;
+    private Vector3 target;
+    private NavMeshAgent agent;
+    private State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        state = State.Roam;
+        target = agent.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        agent.destination = target;
+        Debug.Log(target);
+        //Debug.Log(player.transform.position);
+        Debug.Log(transform.position);
+        switch (state)
+        {
+            case (State.Chase):
+                if (!CanSee()) { state = State.Search; break; }
+                target = player.transform.position;
+                break;
+            case (State.Search):
+                if(CanSee()) { state = State.Chase; break; }
+                SelectRandomDestination(30);
+                
+                break;
+            case (State.Roam):
+                if(CanSee()) { state = State.Chase; break; }
+                SelectRandomDestination(60);
+                break;
+        }
+        
+        /*Debug.Log("transform: "+transform.position);
+        Debug.Log("target: "+ target);
+        if(state == State.Roam && agent.transform.position.x + 5 > target.x && agent.transform.position.x - 5 < target.x && agent.transform.position.z + 5 > target.z && agent.transform.position.z - 5 < target.z)
+        {
+            Debug.Log("true");
+            SelectRandomDestination(30);
+            agent.destination = target;
+        }
+        */
+    }
+
+    private bool CanSee()
+    {
+        Vector3 directionToTarget = player.transform.position - transform.position;
+
+        RaycastHit hit;
+        Physics.Raycast(transform.position, directionToTarget, out hit);
+        if (hit.collider.gameObject.name == "Player")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void SelectRandomDestination(int radius)
+    {
+        if (agent.transform.position.x + 5 > target.x && agent.transform.position.x - 5 < target.x && agent.transform.position.z + 5 > target.z && agent.transform.position.z - 5 < target.z)
+        {
+            Debug.Log("In Range");
+            while (true)
+            {
+                Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * radius;
+
+                // Ensure the random position is within the NavMesh
+                randomDirection += player.transform.position;
+
+                // Sample a valid point on the NavMesh
+                NavMeshHit hit;
+                if (NavMesh.SamplePosition(randomDirection, out hit, radius, NavMesh.AllAreas))
+                {
+                    // Move the NavMeshAgent to the valid position on the NavMesh
+                    target = hit.position;
+                    break;
+                }
+            }
+        }
+        
+    }
+}
